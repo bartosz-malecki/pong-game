c738a2918bcc99d9bbfa553e42ca295277dbeec4
Bartosz Malecki <bartoszmalecki90@gmail.com>
2023-01-24 14:30:48 +0100

    Initial commit. Paddles and ball created.

---
+++ main.py
@@ -0,0 +1,30 @@
+import time
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+ball = Ball()
+screen.listen()
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    ball.move()
+
+screen.exitonclick()
